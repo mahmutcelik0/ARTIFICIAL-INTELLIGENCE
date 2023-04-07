ed1c55d56f95b803ec4a9bfbf36028055c4e6568
MAHMUT CELIK <mahmut.382@hotmail.com>
2023-04-07 18:30:24 +0300

    ROULETT

--- GeneticAlgorithms/src/main/java/org/example/Main.java
+++ GeneticAlgorithms/src/main/java/org/example/Main.java
@@ -3,30 +3,8 @@ package org.example;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-//        PasswordClass passwordClass = new PasswordClass("ChatGPT and GPT-4");
-//
-//        Chromosome chromosome = new Chromosome(geneCount);
-//        chromosome.printChromosome();
-//
-//        System.out.println(passwordClass.fitnessFunction(chromosome.getChromosome()));
+    public static void main(String[] args) throws CloneNotSupportedException {
         Solution solution = new Solution();
         solution.solve();
-
-
-//        Chromosome chromosome1 = new Chromosome(Constans.GENECOUNT.getValue());
-//        Chromosome chromosome2 = new Chromosome(Constans.GENECOUNT.getValue());
-//
-//        System.out.println("BEFORE");
-//        chromosome1.printChromosome();
-//        chromosome2.printChromosome();
-//
-//        char[]temp = chromosome1.getGene();
-//        chromosome1.setSecondHalf(chromosome2.getGene());
-//        chromosome2.setSecondHalf(temp);
-//        System.out.println("AFTER");
-//        chromosome1.printChromosome();
-//        chromosome2.printChromosome();
-
     }
 }
--- GeneticAlgorithms/src/main/java/org/example/Solution.java
+++ GeneticAlgorithms/src/main/java/org/example/Solution.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Solution {
     private final PasswordClass passwordClass = new PasswordClass("ChatGPT and GPT-4");
 
-    public void solve(){
+    public void solve() throws CloneNotSupportedException {
         Generation firstGeneration = new Generation();
         firstGeneration.createFirstGeneration(Constans.CHROMOSOMECOUNT.getValue(),Constans.GENECOUNT.getValue());
         System.out.println("FIRST GENERATION");
@@ -17,13 +17,10 @@ public class Solution {
         secondGeneration.printGeneration();
 
         System.out.println("-----------------------");
-//        firstGeneration.getGeneration().values().forEach(System.out::println);
-//        firstGeneration.roulettWheel().stream().forEach(e-> System.out.println(e.getGene()));
-
         while (secondGeneration.getGeneration().size() < Constans.CHROMOSOMECOUNT.getValue()){
             System.out.println("LOOP:  "+ secondGeneration.getGeneration().size());
             List<Chromosome> temp = firstGeneration.roulettWheel();
-            firstGeneration.makeCrossOver(temp.get(0),temp.get(1)).forEach(e ->{
+            firstGeneration.makeCrossOver((Chromosome) temp.get(0).clone(), (Chromosome) temp.get(1).clone()).forEach(e ->{
                 if(secondGeneration.getGeneration().size() != Constans.CHROMOSOMECOUNT.getValue()){
                     System.out.println(secondGeneration.getGeneration().containsKey(e)); //NASIL AYNI KROMOZOM DENK GELIYOR
                     secondGeneration.getGeneration().put(e, passwordClass.fitnessFunction(e.getGene()));
